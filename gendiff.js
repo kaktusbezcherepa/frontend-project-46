5f993748e2b54b80690595c7526174da5ee331ec
Asstaroth666 <asstaroth6@gmail.com>
2024-05-22 12:10:09 +0300

    lala

--- gendiff.js
+++ gendiff.js
@@ -1,13 +1,31 @@
-import fs from "node:fs"
+import path from "node:path";
+import fs from "node:fs";
 import { Command } from "commander";
+
+
 const program = new Command();
 
-const readFiles = (filepath1, filepath2) => {
-        const firstFile = fs.readFileSync(filepath1, "utf-8")
-        const secondFile = fs.readFileSync(filepath2, "utf-8")
-        console.log(JSON.parse(firstFile));
-        console.log(JSON.parse(secondFile));
-}
+const readFiles = (filepath) => {
+    const pathToFile = path.resolve(filepath);
+    const fileText = fs.readFileSync(pathToFile, "utf-8");
+    return JSON.parse(fileText);
+};
+
+const genDiff = (path1, path2) => {
+    const fileKeys1 = [...Object.keys(readFiles(path1)).sort()];
+    const fileKeys2 = [...Object.keys(readFiles(path2)).sort()];
+    console.log(fileKeys1);
+    console.log(fileKeys2);
+    const obj = fileKeys1
+            .map((value) => {
+                if(value.includes(fileKeys2)){
+                    return {  fileKeys1 : 'test' }
+                }
+            })
+           console.log(obj)
+    
+};
+
 
 program
     .description('Compares two configuration files and shows a difference.')
@@ -15,6 +33,6 @@ program
     .argument('<filepath1>')
     .argument('<filepath2>')
     .option('-f, --format [type]', 'output format')
-    .action((filepath1, filepath2) => readFiles(filepath1, filepath2))
+    .action((filepath1, filepath2) => genDiff(filepath1, filepath2))
 
 program.parse()
